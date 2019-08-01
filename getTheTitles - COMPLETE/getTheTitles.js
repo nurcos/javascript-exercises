50dae55b9ac95c241cd74b6a23af45938cb0c753
nurcos <curson20@hotmail.co.uk>
2019-08-01 16:42:32 +0100

    complete getTheTitles

---
+++ getTheTitles - COMPLETE/getTheTitles.js
@@ -0,0 +1,11 @@
+const getTheTitles = function(books) {
+  let bookTitles = [];
+
+  books.forEach((book)=> {
+    bookTitles.push(book.title);
+  });
+
+  return bookTitles;
+}
+
+module.exports = getTheTitles;
