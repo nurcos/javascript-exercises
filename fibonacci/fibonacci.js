349b533a15d026be82216ad3b82fcf76adbc5aa5
nurcos <curson20@hotmail.co.uk>
2019-08-01 16:15:32 +0100

    complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+  let fromString = +number;
+
+  if(number < 0){
+    return 'OOPS';
+  }
+
+  let lastNumber = 1;
+  let currentNumber = 1;
+
+  let i = 1;
+
+  while(true){
+
+    currentNumber = lastNumber + currentNumber;
+    lastNumber = currentNumber - lastNumber;
+
+    i++;
+
+    if(i == fromString-1){
+      break;
+    }
+  }
+
+  return currentNumber;
 }
 
 module.exports = fibonacci
